6c40a1219b145016087b472be69c061a5f286f65
Ronnie <ronnievella112990@gmail.com>
2017-06-04 07:44:43 +0000

    4.5 carousel error

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,12 +1,12 @@
 class StaticPagesController < ApplicationController
   def index
   end
-end
 
-class StaticPagesController < ApplicationController
+
+
 
   def landing_page
-    @featured_product = Product.limit(3)
+    @featured_product = Product.first(3)
     @products = Product.limit(3)
   end
 
