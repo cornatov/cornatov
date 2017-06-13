b2edbad16ffd4b2b6d17b8802d191d4c8307eb28
Ronnie <ronnievella112990@gmail.com>
2017-06-13 01:59:21 +0000

    Task: 5.10 Admin Role & Moderating User Comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,6 +14,10 @@ def create
   end
     
     def destroy
+      @comment = Comment.find(params[:id])
+      product = @comment.product
+      @comment.destroy
+      redirect_to product
     end
 
 private
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,7 +3,13 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
+    can :read, :all
     can :manage, User, id: user.id
+    if user.admin?
+      can :destroy, :comments
+      can :manage, :all
+    else
+      can :create, :comments
+    end
   end
-
 end
