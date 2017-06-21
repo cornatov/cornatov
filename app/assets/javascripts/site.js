186f137ecb9453400b7953867841935fee429569
Ronnie <ronnievella112990@gmail.com>
2017-06-21 02:41:29 +0000

    6.9 failed attempt

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,11 +1,11 @@
-$(document).on ("turbolinks:load", function() {
-	$('.alert').delay(3000).fadeOut(2000);
-	$('.rating').raty({ path: '/assets', scoreName: 'comment[rating]' });
-	$('.rated').raty({ path: '/assets',
-		readOnly: true,
-		score: function() {
-			return $(this).attr('data-score');
-		}
-	});
-});
+$(document).on('turbolinks:load', function(){
+    $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
+    $('.rated').raty({ path: '/assets',
+      readOnly: true,
+      score: function() {
+        return $(this).attr('data-score');
+      }
+    });
 
+    $(".img-zoom").elevateZoom();
+});
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,6 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
   config.assets.digest = true
-  # https://stackoverflow.com/questions/21359658/rails-4-vendor-asset-images-not-being-referenced-from-css-files-heroku-and-s3
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -56,11 +55,8 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "cfapp_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "firstapp_#{Rails.env}"
   config.action_mailer.perform_caching = false
-
-  # Any dynamic links inside emails will generate the right URLs
-  config.action_mailer.default_url_options = { :host => 'bike-berlin-bmikol.herokuapp.com' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
