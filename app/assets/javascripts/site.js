fd27db07b04b7c96814a2b000c8cf80936d983dc
Ronnie <ronnievella112990@gmail.com>
2017-06-22 02:53:30 +0000

    Task 6.9/6.10 attempt 1

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,11 +1,16 @@
-$(document).on('turbolinks:load', function(){
-    $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
-    $('.rated').raty({ path: '/assets',
-      readOnly: true,
-      score: function() {
-        return $(this).attr('data-score');
-      }
-    });
-
-    $(".img-zoom").elevateZoom();
-});
+var refreshRating = function () { 
+  // Use Raty plugin for comment ratings 
+  $('.rating').raty({ path: '/assets', scoreName: 'comment[rating]' }) 
+  $('.rated').raty({ path: '/assets', 
+  readOnly: true, 
+  score: function () { 
+    return $(this).attr('data-score') 
+  } 
+  }) 
+} 
+  $(document).on('turbolinks:load ajaxSuccess', function () { 
+    refreshRating() 
+  // Make alerts fade-in/fade-out 
+  $('.alert').delay(3000).fadeOut(2000) 
+  // Use elevateZoom plugin for zooming in on images 
+  $('.img-zoom').elevateZoom({ zoomType: 'inner', cursor: 'crosshair' }) })
--- vendor/assets/javascripts/site.js
+++ vendor/assets/javascripts/site.js
@@ -1,11 +1,23 @@
-$(document).on ("turbolinks:load", function() {
-	$('.alert').delay(3000).fadeOut(2000);
-	$('.rating').raty({ path: '/assets', scoreName: 'comment[rating]' });
-	$('.rated').raty({ path: '/assets',
-		readOnly: true,
-		score: function() {
-			return $(this).attr('data-score');
-		}
-	});
-});
+var refreshRating = function() {
+  $('.rating').raty({
+    starHalf: '<%= asset_path 'star-half.png' %>',
+    starOff: '<%= asset_path 'star-off.png' %>',
+    starOn: '<%= asset_path 'star-on.png' %>',
+    scoreName: 'comment[rating]'
+  });
+    $('.rated').raty({
+    starHalf: '<%= asset_path 'star-half.png' %>',
+    starOff: '<%= asset_path 'star-off.png' %>',
+    starOn: '<%= asset_path 'star-on.png' %>',
+    readOnly: true,
+    score: function() {
+      return $(this).attr('data-score');
+    }
+  });
+};
 
+$(document).on('turbolinks:load', function() {
+    refreshRating();
+
+    $('.img-zoom').elevateZoom();
+});
