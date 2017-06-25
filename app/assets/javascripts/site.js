ff996a0a746c5b34c84a629017811498452cf048
Ronnie <ronnievella112990@gmail.com>
2017-06-25 21:56:18 +0000

    Task 6.10 fixes

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,16 +1,18 @@
-var refreshRating = function () { 
-  // Use Raty plugin for comment ratings 
-  $('.rating').raty({ path: '/assets', scoreName: 'comment[rating]' }) 
-  $('.rated').raty({ path: '/assets', 
-  readOnly: true, 
-  score: function () { 
-    return $(this).attr('data-score') 
-  } 
-  }) 
-} 
-  $(document).on('turbolinks:load ajaxSuccess', function () { 
-    refreshRating() 
-  // Make alerts fade-in/fade-out 
-  $('.alert').delay(3000).fadeOut(2000) 
-  // Use elevateZoom plugin for zooming in on images 
-  $('.img-zoom').elevateZoom({ zoomType: 'inner', cursor: 'crosshair' }) })
+var refreshRating = function(){
+  $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
+  $('.rated').raty({ path: '/assets',
+    readOnly: true,
+    score: function() {
+      return $(this).attr('data-score');
+    }
+  });
+ };
+ $(document).on('turbolinks:load', function() {
+    refreshRating();
+ 
+  $('.img-zoom').elevateZoom({
+    zoomWindowWidth:300,
+    zoomWindowHeight:200
+
+  });
+});
