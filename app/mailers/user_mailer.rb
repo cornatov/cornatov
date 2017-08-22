2f44d5761f68143e1244408ba351c894211063fb
Ronnie <cornatov@buffalo.edu>
2017-08-22 03:51:26 +0000

    fixes 8/21/17

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,14 +5,14 @@ class UserMailer < ApplicationMailer
   @message = message
     mail(:from => email,
          :to => 'cornatov@buffalo.edu',
-         :subject => "A new contact form message from #{name}")
+         :subject => "A new contact form message from #{user.first_name}")
   end
 
 def welcome_email(user)
      @user = user
        mail(:from => 'cornatov@buffalo.edu',
          :to => user.email,
-         :subject => "Welcome and thank you #{name} for joining!")
+         :subject => "Welcome and thank you #{user.first_name} for joining!")
  end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
     resources :comments
   end
   
-  resources :users
+  
   
   get 'static_pages/about'
 
